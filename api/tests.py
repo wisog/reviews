04327182d8b5a73080b02d365caa32a134471047
wisog <cesar.vela@gmail.com>
2018-10-31 20:59:12 -0600

    tests

--- api/tests.py
+++ api/tests.py
@@ -34,10 +34,18 @@ class ModelsTest(TestCase):
         self.assertEqual(str(review.title), 'title')
         self.assertEqual(review.user, user)
 
-    def test_create_review_bad_parameters(self):
+    def test_get_review_bad_parameters(self):
         user = self.create_user('user2', 'pepe@algo.com', 'test')
         response = self.client.get('/api/v1/reviews/')
         self.assertEqual(response.status_code, 401)
+
+    def test_get_reviews(self):
+        token = self.client.post('/api/v1/token/', {"username": 'cesar', "password": 'qwerty'})
+        token_data = json.loads(token.content.decode("utf-8"))
+        access_token = token_data['access']
+        self.client.defaults['HTTP_AUTHORIZATION'] = 'Bearer ' + access_token
+        response = self.client.get('/api/v1/reviews/')
+        print(response.content)
 
     def test_get_user_token(self):
         token = self.client.post('/api/v1/token/', {"username": 'cesar', "password": 'qwerty'})
@@ -57,4 +65,3 @@ class ModelsTest(TestCase):
         self.client.defaults['HTTP_AUTHORIZATION'] = 'Bearer ' + access_token
         review = self.client.get('/api/v1/reviews/1/')
         self.assertTrue(review.status_code != 401)
-
